b86c545f05244d5a4f5fe473d7135f2c23b089f1
Neo8 <56829999+Keksnet@users.noreply.github.com>
2022-10-02 20:16:29 +0200

    .

--- src/main/java/de/neo/prodtp/util/TPARequest.java
+++ src/main/java/de/neo/prodtp/util/TPARequest.java
@@ -16,10 +16,10 @@ import org.bukkit.entity.Player;
 
 public class TPARequest {
 	
-	private UUID sender;
-	private UUID receiver;
-	private boolean tpahere;
-	private long time;
+	private final UUID sender;
+	private final UUID receiver;
+	private final boolean tpahere;
+	private final long time;
 	
 	public TPARequest(UUID sender, UUID receiver) {
 		this(sender, receiver, false);
@@ -33,89 +33,57 @@ public class TPARequest {
 	}
 	
 	public void accept() {
-		OfflinePlayer s = Bukkit.getOfflinePlayer(this.sender);
-		OfflinePlayer r = Bukkit.getOfflinePlayer(this.receiver);
-		if(this.tpahere) {
-			if(s.isOnline() && r.isOnline()) {
-				s.getPlayer().sendMessage(ProdTPMain.getMessage("tpahere_accepted").replace("%player%", r.getName()));
-				TPUtil.safeTP(r.getPlayer(), s.getPlayer());
-				ProdTPPlayer tpPlayer = ProdTPMain.getInstance().getProdTPPlayerManager().get(s.getUniqueId());
-				tpPlayer.setCooldown(r.getUniqueId(), ProdTPMain.getInstance().getConfig().getInt("options.tpa_cooldown"));
-			}else if(s.isOnline()) {
-				s.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
-			}else if(r.isOnline()) {
-				r.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
-			}
-		}else {
-			if(s.isOnline() && r.isOnline()) {
-				s.getPlayer().sendMessage(ProdTPMain.getMessage("tpa_accepted").replace("%player%", r.getName()));
-				TPUtil.safeTP(s.getPlayer(), r.getPlayer());
-				ProdTPPlayer tpPlayer = ProdTPMain.getInstance().getProdTPPlayerManager().get(s.getUniqueId());
-				tpPlayer.setCooldown(r.getUniqueId(), ProdTPMain.getInstance().getConfig().getInt("options.tpa_cooldown"));
-			}else if(s.isOnline()) {
-				s.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
-			}else if(r.isOnline()) {
-				r.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
-			}
+		OfflinePlayer sender = Bukkit.getOfflinePlayer(this.sender);
+		OfflinePlayer receiver = Bukkit.getOfflinePlayer(this.receiver);
+		if(sender.isOnline() && receiver.isOnline()) {
+			sender.getPlayer().sendMessage(ProdTPMain.getMessage("tpa" + (tpahere ? "here" : "") + "_accepted")
+					.replace("%player%", receiver.getName()));
+			TPUtil.safeTP((tpahere ? receiver : sender).getPlayer(), (tpahere ? sender : receiver).getPlayer());
+			ProdTPPlayer tpPlayer = ProdTPMain.getInstance().getProdTPPlayerManager().get(sender.getUniqueId());
+			tpPlayer.setCooldown(receiver.getUniqueId(), ProdTPMain.getInstance().getConfig()
+					.getInt("options.tpa_cooldown"));
+		}else if(sender.isOnline()) {
+			sender.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
+		}else if(receiver.isOnline()) {
+			receiver.getPlayer().sendMessage(ProdTPMain.getMessage("expired"));
 		}
 	}
 	
 	public void sendInvite() {
 		if(!ProdTPMain.getInstance().getProdTPPlayerManager().get(this.receiver).isTpBlocked()) {
-			OfflinePlayer r = Bukkit.getOfflinePlayer(this.receiver);
-			OfflinePlayer s = Bukkit.getOfflinePlayer(this.sender);
-			if(r.isOnline() && s.isOnline()) {
-				FileConfiguration config = ProdTPMain.getInstance().getConfig();
-				ProdTPPlayer tpPlayer = ProdTPMain.getInstance().getProdTPPlayerManager().get(s.getUniqueId());
-				if(tpPlayer.getTpaTime() <= System.currentTimeMillis()) {
-					Player p = (Player) s.getPlayer();
-					p.sendMessage(ProdTPMain.getMessage("on_cooldown"));
-					return;
-				}
-				tpPlayer.setTpaTime(System.currentTimeMillis() + (config.getInt("options.tpa_sent_cooldown") * 1000L));
+			OfflinePlayer receiver = Bukkit.getOfflinePlayer(this.receiver);
+			OfflinePlayer sender = Bukkit.getOfflinePlayer(this.sender);
+			FileConfiguration config = ProdTPMain.getInstance().getConfig();
+			ProdTPPlayer tpPlayer = ProdTPMain.getInstance().getProdTPPlayerManager().get(sender.getUniqueId());
+			if(tpPlayer.getTpaTime() <= System.currentTimeMillis()) {
+				Player p = sender.getPlayer();
+				p.sendMessage(ProdTPMain.getMessage("on_cooldown"));
+				return;
+			}
+			tpPlayer.setTpaTime(System.currentTimeMillis() + (config.getInt("options.tpa_sent_cooldown") * 1000L));
 
-				BaseComponent[] texts_arr = TextComponent.fromLegacyText(ProdTPMain.getInstance().getConfig().getString("messages.accepttpa"));
-				ArrayList<BaseComponent> texts = new ArrayList<>();
-				if(this.tpahere) {
-					for (BaseComponent elem : texts_arr) {
-						elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§aKlicke hier zum akzeptieren")));
-						elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpaccept " + s.getName()));
-						texts.add(elem);
-					}
-					BaseComponent[] tmp_arr = TextComponent.fromLegacyText(ProdTPMain.getInstance().getConfig().getString("messages.denytpa"));
-					ArrayList<BaseComponent> tmp = new ArrayList<BaseComponent>();
-					for (BaseComponent elem : tmp_arr) {
-						elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§cKlicke hier zum ablehnen")));
-						elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpdeny " + s.getName()));
-						tmp.add(elem);
-					}
-					texts.addAll(tmp);
-					r.getPlayer().spigot().sendMessage(TextComponent.fromLegacyText(ProdTPMain.getMessage("tpahere").replace("%player%", s.getName())));
-					r.getPlayer().spigot().sendMessage(texts.toArray(new BaseComponent[0]));
-					s.getPlayer().sendMessage(ProdTPMain.getMessage("sent"));
-				}else {
-					for (BaseComponent elem : texts_arr) {
-						elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§aKlicke hier zum akzeptieren")));
-						elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpaccept " + s.getName()));
-						texts.add(elem);
-					}
-					BaseComponent[] tmp_arr = TextComponent.fromLegacyText(ProdTPMain.getInstance().getConfig().getString("messages.denytpa"));
-					ArrayList<BaseComponent> tmp = new ArrayList<BaseComponent>();
-					for (BaseComponent elem : tmp_arr) {
-						elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§cKlicke hier zum ablehnen")));
-						elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpdeny " + s.getName()));
-						tmp.add(elem);
-					}
-					texts.addAll(tmp);
-					r.getPlayer().spigot().sendMessage(TextComponent.fromLegacyText(ProdTPMain.getMessage("tpa").replace("%player%", s.getName())));
-					r.getPlayer().spigot().sendMessage(texts.toArray(new BaseComponent[0]));
-					s.getPlayer().sendMessage(ProdTPMain.getMessage("sent"));
-				}
+			BaseComponent[] texts_arr = TextComponent.fromLegacyText(ProdTPMain.getMessage("accepttpa"));
+			ArrayList<BaseComponent> texts = new ArrayList<>();
+			for (BaseComponent elem : texts_arr) {
+				elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§aKlicke hier zum akzeptieren")));
+				elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpaccept " + sender.getName()));
+				texts.add(elem);
 			}
+			BaseComponent[] tmp_arr = TextComponent.fromLegacyText(ProdTPMain.getMessage("denytpa"));
+			ArrayList<BaseComponent> tmp = new ArrayList<>();
+			for (BaseComponent elem : tmp_arr) {
+				elem.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("§cKlicke hier zum ablehnen")));
+				elem.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/tpdeny " + sender.getName()));
+				tmp.add(elem);
+			}
+			texts.addAll(tmp);
+			receiver.getPlayer().spigot().sendMessage(TextComponent
+					.fromLegacyText(ProdTPMain.getMessage(this.tpahere ? "tpahere" : "tpa")
+							.replace("%player%", sender.getName())));
+			receiver.getPlayer().spigot().sendMessage(texts.toArray(new BaseComponent[0]));
+			sender.getPlayer().sendMessage(ProdTPMain.getMessage("sent"));
 		}else {
-			if(Bukkit.getOfflinePlayer(this.sender).isOnline()) {
-				Bukkit.getOfflinePlayer(this.sender).getPlayer().sendMessage(ProdTPMain.getMessage("tp_blocked"));
-			}
+
 		}
 	}
 	
